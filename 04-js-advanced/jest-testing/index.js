64a9677288c7b1525ec60c95e1d05fc07938a0bc
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2021-09-25 12:39:58 +0100

    testing error

---
+++ 04-js-advanced/jest-testing/index.js
@@ -0,0 +1,5 @@
+export const getName = (name, age) => {
+    return `${name}, ${age} years old.`
+}
+
+console.log(getName ("Sam",22));
---
+++ 04-js-advanced/jest-testing/index.test.js
@@ -0,0 +1,6 @@
+import { getName } from "./index.js";
+
+it("Should return the string'Sam, 22 years old.'"), () => {
+    output = getName("Sam", 22);
+    expect(output).toBe("Sam, 22 years old.");
+};
